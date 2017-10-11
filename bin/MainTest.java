2bdb8978f9f8e54239954b0f32e2dffd3d4065ae
qzy <1437579368@qq.com>
2017-10-11 22:29:37 +0800

    单元测试

---
+++ bin/MainTest.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+
+
+public class MainTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testLengthOfSubString() {
+		fail("Not yet implemented");
+	}
+
+}
